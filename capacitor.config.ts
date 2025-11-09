62839be0ea3fe02d048050da407604a6de30c069
lcx2011 <2743566355@qq.com>
2025-11-09 09:02:51 +0800

    加入安卓试试

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.example.musicapp',
+  appName: 'music-app',
+  webDir: 'dist'
+};
+
+export default config;
